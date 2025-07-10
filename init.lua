5cf4b784d60f51f66c7451a5c960952103603245
Danil <ershov200112@gmail.com>
2025-07-10 09:47:23 +0300

    minor changes

--- init.lua
+++ init.lua
@@ -1,20 +1,3 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
-end
-vim.opt.rtp:prepend(lazypath)
-
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
@@ -39,6 +22,23 @@ vim.o.showmode = false
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
 -- Setup lazy.nvim
 require("lazy").setup({
 	spec = {
@@ -52,18 +52,17 @@ require("lazy").setup({
 		-- Keybindings hint
 		{
 			"folke/which-key.nvim",
-			event = "VimEnter",
+			event = "VeryLazy",
 			opts = {
-				delay = 0,
-				icons = {
-					mappings = vim.g.have_nerd_font,
-				},
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
 			},
 			keys = {
 				{
 					"<leader>?",
 					function()
-						require("which-key").show({ global = true })
+						require("which-key").show({ global = false })
 					end,
 					desc = "Buffer Local Keymaps (which-key)",
 				},
